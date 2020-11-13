d616ad50634fc54d3a4ecb04370199b8dec5657d
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2020-11-13 14:41:00 +0100

    Homepage with posts and tracing

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,15 +1,48 @@
 // src/pages/home/HomePage.tsx
-import React from "react";
+import { observer } from "mobx-react-lite";
+import { trace } from 'mobx'
+import React, { useContext } from "react";
 
-import { Typography, Container } from "@material-ui/core";
+import { Typography, Container, Grid, Card, CardContent } from "@material-ui/core";
+import { RootStoreContext } from "../../stores/RootStore";
 
-export default function HomePage() {
+export default observer(() => {
+  const { blogPostStore } = useContext(RootStoreContext);
+
+  trace()
+
   return (
     <Container fixed>
       <Typography variant="h3" component="h1">
         Codaisseur Coders Network
       </Typography>
-      <p>Welcome!</p>
+      {blogPostStore.isLoading && <p>Loading...</p>}
+      {!blogPostStore.isLoading && (
+        <Grid container spacing={3}>
+          {blogPostStore.blogPosts.map((post) => {
+            return (
+              <Grid key={post.id} item xs={4}>
+                <Card>
+                  <CardContent
+                    style={{ maxHeight: "15rem", overflow: "hidden" }}
+                  >
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {post.title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {post.content}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </Container>
   );
-}
+});
